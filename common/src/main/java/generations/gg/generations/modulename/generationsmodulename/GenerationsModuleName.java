fada5589fa0743e171f2a62daabb85fe90960292
Joseph T. McQuigg <j.t.mcquigg12@gmail.com>
2023-07-02 17:29:33 -0400

    Add Example Config
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- common/src/main/java/generations/gg/generations/modulename/generationsmodulename/GenerationsModuleName.java
+++ common/src/main/java/generations/gg/generations/modulename/generationsmodulename/GenerationsModuleName.java
@@ -1,8 +1,18 @@
 package generations.gg.generations.modulename.generationsmodulename;
 
-public class GenerationsModuleName {
-    public static final String MOD_ID = "generationsmodulename";
-    public static void init() {
 
+import generations.gg.generations.core.generationscore.config.ConfigLoader;
+import generations.gg.generations.modulename.generationsmodulename.config.Config;
+
+public class GenerationsModuleName {
+
+    /** The mod id for the Generations-ModuleName mod. */
+    public static final String MOD_ID = "generationsmodulename";
+
+    /** The config for the Generations-ModuleName mod. */
+    public static Config CONFIG;
+
+    public static void init() {
+        CONFIG = ConfigLoader.loaderConfig(Config.class, "name", "config");
     }
 }
